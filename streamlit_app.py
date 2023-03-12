5c73d8858d6d9539eea932eb0606a9c127c9d267
dchawla2023 <127057835+dchawla2023@users.noreply.github.com>
2023-03-12 17:08:48 -0400

    Update streamlit_app.py
    change to header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
