490374b3fb40363eacf1778853e1dd887ef778b7
dchawla2023 <127057835+dchawla2023@users.noreply.github.com>
2023-03-14 19:33:29 -0400

    Update streamlit_app.py
    Formatted the code and added new functions. also close connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ try:
 except URLError as e:
  streamlit.error(e)
 
-streamlit.header("View out Fruit list, Add your Favourite!")
+streamlit.header("View Our Fruit List, Add Your Favourite!")
 def get_fruit_load_list():
  with my_cnx.cursor() as my_cur:
   my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
